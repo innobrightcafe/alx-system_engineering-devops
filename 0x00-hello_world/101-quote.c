d8bf6995a009b17d7607cffe7dc5c06807dbfbfe
innobrightcafe <innobrightcafe@gmail.com>
2022-09-09 18:13:30 +0000

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,36 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Return: Always 0 (Success)
  */
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-
-#define RED "\e[0;31m"
-#define NC "\e[0m"
-
-
-int main(int argc, char *argv[]){
-
-    if (argc != 2) {
-        fwrite("and that piece of art is useful" - Dora Korpar, 2015-10-19 \n", 39, 1, stderr);
-        exit(EXIT_FAILURE);
-    }
-
-    char *str = malloc(strlen(argv[1]) + 1);
-    strcpy(str, argv[1]);
-
-    printf("str: %s\n", str);
-
-    free(str);
-    exit(EXIT_SUCCESS);
-}
-
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful" - Dora Korpar, 2015-10-19 \n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
