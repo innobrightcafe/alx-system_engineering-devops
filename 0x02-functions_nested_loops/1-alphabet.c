b0f495fc59e34d76a854c897f0f296f4aa6f9a5a
innobrightcafe <innobrightcafe@gmail.com>
2022-09-13 14:40:33 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,11 @@
  *
  * Return: Always 0 (success)
  */
+
 void print_alphabet(void)
 {
 	char alphabet;
-	for (alphabet-'a'; alphabet<= 'z'; alphabet++)
+	for (alphabet='a'; alphabet<= 'z'; alphabet++)
 	{
 		_putchar (alphabet);
 	}
