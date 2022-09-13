01914f1413231528b00c11a861944eabb19b12db
innobrightcafe <innobrightcafe@gmail.com>
2022-09-13 15:20:24 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,14 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase
  *
- *
  * Return: Always 0 (success)
+ *
  */
 
 void print_alphabet(void)
 {
 	char alphabet;
-	for (alphabet='a'; alphabet<= 'z'; alphabet++)
+	for (alphabet ='a'; alphabet <= 'z'; alphabet++)
 	{
 		_putchar (alphabet);
 	}
