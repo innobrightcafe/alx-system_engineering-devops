7a7ec854a4080d6fe6688651ddde132bb3cfd0cc
innobrightcafe <innobrightcafe@gmail.com>
2022-09-13 16:29:41 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 X the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	char alphabet, co;
+
+	co = 0;
+
+	while (co < 10)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphaet++)
+		{
+			_putchar (alphabet);
+		}
+		co++;
+		_putchar('\n');
+	}
+}
