606a3ba10c21868def84e682fcc28d7c3234781a
innobrightcafe <innobrightcafe@gmail.com>
2022-09-15 07:03:02 +0000

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - will test function to print integer if its + or -
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
