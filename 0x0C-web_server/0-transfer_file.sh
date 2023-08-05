16b9693c4552d9513e36c38a1f989cd660e2bf13
inosoft <innobrightcafe@gmail.com>
2023-08-05 12:00:36 -0400

    devops

--- 0x0C-web_server/0-transfer_file.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Set variables for the client, server, and file
-client="inosoft@197.210.85.39"
-server="ubuntu@100.25.17.237"
-file="/path/to/file"
-# Disable strict host key checking for this session only
-ssh_options="-o StrictHostKeyChecking=no"
-# Transfer the file from the client to the server using scp
-scp "$file" "$server:~/"
